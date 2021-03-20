809ef54d0c36b8bfb66267853206cc6b93b151ff
David Haynes <david.m.haynes@gmail.com>
2021-03-19 20:43:29 -0400

    added config and cleaned up code

--- hrpm.js
+++ hrpm.js
@@ -1,37 +1,36 @@
 const express = require('express')
+
 const expressHandlebars = require('express-handlebars')
-const port = process.env.PORT || 3000
+
+const handlers = require('./lib/handlers')
+
 const app = express()
-const fortune = require('./lib/fortune')
 
 // configure Handlebars view engine
 app.engine('handlebars', expressHandlebars({
-    defaultLayout: 'main',
+  defaultLayout: 'main',
 }))
+app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
-app.set('view engine', 'handlebars')
+app.get('/', handlers.home)
 
-app.get('/', (req, res) => res.render('home'))
-
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
 // custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
-    `Express started on http://localhost:${port}; ` +
-    `press Ctrl-C to terminate.`))
+if(require.main === module) {
+  app.listen(port, () => {
+    console.log( `Express started on http://localhost:${port};` +
+      ' press Ctrl-C to terminate.' )
+  })
+} else {
+  module.exports = app
+}
